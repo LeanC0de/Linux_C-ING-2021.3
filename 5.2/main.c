8cf6f653e20b422cd9a108e7f8327e883ef43afb
Joshua Chang <32766984+LeanC0de@users.noreply.github.com>
2023-12-06 22:19:09 +0800

    5.2 Incremental development

---
+++ 5.2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+double distance(double x1, double y1, double x2, double y2)
+{ 
+    return 0.0;
+}
+
+int main(void)
+{
+   printf("distance is %f\n", distance(1.0, 2.0, 4.0, 6.0));
+   return 0;
+}
